af46ee8966cae283c96ba9e15b289a895a8ec786
Tabea Röthemeyer <tabea.roethemeyer@gruppe.ai>
2025-01-30 16:06:37 +0100

    create inital setup of app component
    The layout and structure are used for a preliminary layout of the website, using the components for selection and data displaying
    already created. Configure the app.config such that all necessary stuff like HTTP clients exist

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DeviceSelectionComponent } from './device-handling/device-selection/device-selection.component';
+import { GraphComponent } from './graph/graph/graph.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [DeviceSelectionComponent, GraphComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-frontend';
