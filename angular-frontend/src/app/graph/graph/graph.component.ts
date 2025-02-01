d53117059f84b49f71e0cd5d477615a5ccd1a47c
Tabea Röthemeyer <tabea.roethemeyer@gruppe.ai>
2025-02-01 12:48:23 +0100

    resize graph to outer div
    I cant fully explain why this works but all the code seems to be needed to make sure that the graph is usable
    and resizes as expected to changes in the size

--- angular-frontend/src/app/graph/graph/graph.component.ts
+++ angular-frontend/src/app/graph/graph/graph.component.ts
@@ -5,6 +5,7 @@ import {
   effect,
   ElementRef,
   input,
+  signal,
   viewChild,
 } from '@angular/core';
 import { newPlot, react } from 'plotly.js-basic-dist-min';
@@ -12,8 +13,25 @@ import { newPlot, react } from 'plotly.js-basic-dist-min';
 @Component({
   selector: 'app-graph',
   imports: [CommonModule],
-  templateUrl: `./graph.component.html`,
-  styles: ``,
+  template: `<div class="size-full border border-blue-500">
+    <div #plotlyContainer class="size-full"></div>
+  </div> `,
+  styles: [
+    `
+      :host {
+        display: flex;
+        flex: 1;
+        width: 100%;
+        height: 100%;
+      }
+    `,
+    `
+      .graph-container {
+        width: 100%;
+        height: 100%;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GraphComponent {
@@ -21,24 +39,39 @@ export class GraphComponent {
     input.required<Record<string, { timestamp: number; value: number }[]>>();
 
   plotContainer = viewChild.required<ElementRef>('plotlyContainer');
+  containerSize = signal<{ width: number; height: number }>({
+    width: 0,
+    height: 0,
+  });
+
   updatGraph = effect(() => {
     const data = this.dataSeries();
-    this.updateGraph();
+    const size = this.containerSize();
+    this.updateGraph(size.width, size.height);
   });
   ngAfterViewInit(): void {
     this.initializeGraph();
   }
 
   initializeGraph() {
+    const element = this.plotContainer().nativeElement;
+    this.containerSize.set({
+      width: element.clientWidth,
+      height: element.clientHeight,
+    });
+
     newPlot(this.plotContainer().nativeElement, [], {
       title: 'Oszillogramm',
       xaxis: { title: 'Zeit (ms)' },
       yaxis: { title: 'Wert' },
       showlegend: true,
+      autosize: false,
+      width: element.width,
+      height: element.height,
     });
   }
 
-  updateGraph() {
+  updateGraph(width: number, height: number) {
     const traces = Object.entries(this.dataSeries()).map(([uuid, points]) => ({
       x: points.map((p) => p.timestamp),
       y: points.map((p) => p.value),
@@ -46,6 +79,6 @@ export class GraphComponent {
       name: uuid,
     }));
 
-    react(this.plotContainer().nativeElement, traces);
+    react(this.plotContainer().nativeElement, traces, {}, { responsive: true });
   }
 }
